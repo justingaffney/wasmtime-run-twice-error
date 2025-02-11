8fa208dc34d34a79df7fe29893438a135fa971e6
Justin Gaffney <justingaffney@hotmail.com>
2025-02-11 20:12:55 +1000

    Add example

---
+++ host/src/main.rs
@@ -0,0 +1,58 @@
+use wasmtime::{
+    Engine,
+    Store,
+    component::{
+        Component,
+        ResourceTable,
+        Linker
+    }
+};
+use wasmtime_wasi::{
+    WasiView,
+    WasiCtxBuilder,
+    WasiCtx
+};
+
+fn main() {
+    // Create component instance
+    let engine = Engine::default();
+    let component = Component::from_file(&engine, "../target/wasm32-wasip1/release/guest.wasm").unwrap();
+    let resource_table = ResourceTable::new();
+    let wasi_ctx = WasiCtxBuilder::new().inherit_stdout().build();
+    let state = GuestState { resource_table, wasi_ctx };    
+    let mut store = Store::new(&engine, state);    
+    let mut linker = Linker::new(&engine);
+    wasmtime_wasi::add_to_linker_sync(&mut linker).unwrap();
+    let instance = linker.instantiate(&mut store, &component).unwrap();
+
+    // Get run function
+    let run_interface_idx = instance.get_export(&mut store, None, "wasi:cli/run@0.2.0").unwrap();
+    let run_func_idx = instance.get_export(&mut store, Some(&run_interface_idx), "run").unwrap();
+    let run_func = instance.get_func(&mut store, run_func_idx).unwrap();
+    let run_func = run_func.typed::<(), (Result<(), ()>,)>(&mut store).unwrap();
+
+    // Call run function
+    println!("Calling run function for the first time");
+    run_func.call(&mut store, ()).unwrap().0.unwrap();
+    run_func.post_return(&mut store).unwrap();
+    
+    // Call run function again
+    println!("Calling run function for the second time");
+    run_func.call(&mut store, ()).unwrap().0.unwrap();
+    run_func.post_return(&mut store).unwrap();
+}
+
+struct GuestState {
+    resource_table: ResourceTable,
+    wasi_ctx: WasiCtx,
+}
+
+impl WasiView for GuestState {
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.resource_table
+    }
+
+    fn ctx(&mut self) -> &mut WasiCtx {
+        &mut self.wasi_ctx
+    }
+}
